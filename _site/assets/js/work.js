6321fbb4b32cf1948b6f3771c8bdfc32ff701adb
Debjeet Das <debjeet.das@hashworks.co>
2018-03-19 15:04:15 +0530

    practice.html update

---
+++ _site/assets/js/work.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    
+    $(".lb_work_filterli").click(function(){
+        $(this).addClass('lb_work_filterActive');
+        $(this).siblings().removeClass('lb_work_filterActive');
+    });
+});
