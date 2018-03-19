0dadc1dfe142c8e770e8af8851769448a1043e6f
Debjeet Das <debjeet.das@hashworks.co>
2018-03-19 14:42:55 +0530

    style.scss update

--- _site/assets/js/work.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-    
-    $(".lb_work_filterli").click(function(){
-        $(this).addClass('lb_work_filterActive');
-        $(this).siblings().removeClass('lb_work_filterActive');
-    });
-});
